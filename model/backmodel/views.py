0c630860fa34158ed83fc49bce3a12fb8987cf50
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-03-21 21:15:15 -0700

    model connected to website

--- model/backmodel/views.py
+++ model/backmodel/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from joblib import load
 
+md = load("backmodel/ML_model/email_model.joblib")
 # Create your views here.
 def start(request):
     return render(request, 'index.html')
 
-def msg(request):
-    return render(request, 'msg.html')
+def dataGiven(request):
+    e_body = request.GET['email_b']
+    y_pred = md.predict([e_body])
+    print(y_pred)
+    return render(request, 'index.html', {'answer' : y_pred})
