2f898bfeea83a2dcd1e6abda898e0f10f21b31d2
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2025-03-23 21:53:52 -0700

    testing render

--- model/settings.py
+++ model/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-8@e3hrferlc*e(-m6=sa%ibe9vzzw)mcl)a_ik!5#4^h%&$5$$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -110,3 +110,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
